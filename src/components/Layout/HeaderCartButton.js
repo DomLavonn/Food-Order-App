a7bc8f57385b90ba3797a899559bb4a39042f534
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-15 18:36:33 -0500

    Added cart icon

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,2 +1,11 @@
 import React from "react";
 
+const HeaderCartButton = () => {
+    return <button>
+        <span></span>
+        <span></span>
+        <span></span>
+    </button>
+}
+
+export default HeaderCartButton
