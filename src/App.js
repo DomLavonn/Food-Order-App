741e16f031b88fdcc1afe0f1e62622ad6ed46b9c
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-15 18:26:52 -0500

    Added new files as well as style classes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
-    <div className="App">
-     <h1>hello</h1>
-    </div>
+    <>
+     <Header/>
+    </>
   );
 }
 
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,1 +1,18 @@
 import React from "react";
+import mealsImage from '../../assets/mealimg.jpg'
+import classes from './Header.module.css'
+
+const Header = (props) => {
+    return <>
+        <header className={classes.header}>
+            <h1> React Meals</h1>
+            <button>Cart</button>
+            <button></button>
+        </header>
+        <div className={classes['main-image']}>
+            <img src={mealsImage} alt="Table full of delicious food"/>
+        </div>
+    </>
+}
+
+export default Header
