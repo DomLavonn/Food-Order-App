9b93d170da0befcfa9f96c98220630cb1b3678eb
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-16 12:33:05 -0500

    Creating modal

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,12 +1,24 @@
 import classes from './Cart.module.css'
 
 const Cart = (props) => {
-    const cartItem = [{id: 'c1', name: 'Shsho', amount: 2, price: 12.99}]
+    // getting our items list and store in variable
+    const cartItem =<ul className={classes['card-items']}>{[
+        {id: 'c1', name: 'Shsho', amount: 2, price: 12.99
+    }].map((item) => <li>{item.name}</li>)
+    }</ul>
 
     return <div>
         cartitems
-        <div></div>
-        <div></div>
+        <div className={classes.total}>
+            <span>Total Amount</span>
+            <span>35.66</span>
+        </div>
+
+        <div className={classes.actions}>
+            <button className={classes['button--alt']}> Close </button>
+            <button className={classes.button}> Order </button>
+
+        </div>
     </div>
 }
 
