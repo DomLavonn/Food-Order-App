07814994345e021965751d780bcc8b9abe18931e
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-16 11:53:45 -0500

    push

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -2,7 +2,10 @@ import classes from './MealItemForm.module.css'
 
 
 const MealItemForm = () => {
-
+    return <form className={classes.form}>
+        <input type="text"/>
+        <button>+ Add</button>
+    </form>
 }
 
 export default MealItemForm
