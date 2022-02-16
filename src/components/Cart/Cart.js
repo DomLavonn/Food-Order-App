2de2eb9e322bdf2f952eaa39846a4e97ea0c30af
DomLavonn <99680793+DomLavonn@users.noreply.github.com>
2022-02-16 12:23:51 -0500

    pushing 3

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,13 @@
+import classes from './Cart.module.css'
+
+const Cart = (props) => {
+    const cartItem = [{id: 'c1', name: 'Shsho', amount: 2, price: 12.99}]
+
+    return <div>
+        cartitems
+        <div></div>
+        <div></div>
+    </div>
+}
+
+export default Cart
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,9 +1,16 @@
 import classes from './MealItemForm.module.css'
+import Input from "../../UI/Input/Input";
 
-
-const MealItemForm = () => {
+const MealItemForm = (props) => {
     return <form className={classes.form}>
-        <input type="text"/>
+        <Input label={"Amount"} input={{
+            id: 'amount_' + props.id,
+            type: 'number',
+            min: '1',
+            mas: '5',
+            step: '1',
+            defaultValue: '1'
+        }} />
         <button>+ Add</button>
     </form>
 }
